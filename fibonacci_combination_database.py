929d83e7ec228bc0c88285fdca893e55c8575ec4
ga53veh <mahmoud.ammar@tum.de>
2020-10-20 10:57:03 +0200

    add comments for fibonacci_combination_database.py

--- fibonacci_combination_database.py
+++ fibonacci_combination_database.py
@@ -3,6 +3,16 @@ import sqlite3
 
 
 class FibonacciCombinationDatabase:
+    """
+    class FibonacciCombinationDatabase to add calculated
+    combinations to the database
+
+    Attributes
+    ----------
+    get_combination : list[list]
+        the generated combinations sums of the fibonacci 
+        sequence which are saved in the database.
+    """
 
     def __init__(self):
         self.conn = sqlite3.connect('combinations.db')
@@ -16,10 +26,32 @@ class FibonacciCombinationDatabase:
         self.conn.commit()
 
     def add_combination(self, number, combination):
+        """
+        adds the number and its calculated combination 
+        into the database table
+
+        Parameters
+        ----------
+        number : int
+            the number used to to find the combination sums
+        combination : int
+            the list containing the combination sum
+        """
+
         self.execute(
             '''INSERT INTO Combinations (number, combination) VALUES (?,?)''', (number, combination))
 
     def get_combination(self, num):
+        """
+        gets the generated combination of variable number from
+        the database table. Returns None if it doesn't exist
+
+        Parameters
+        ----------
+        num : int
+            the number used to to find the combination sums
+        """
+
         self.execute(
             '''SELECT combination FROM Combinations WHERE number = ? ''', (num,))
         return self.cur.fetchone()
